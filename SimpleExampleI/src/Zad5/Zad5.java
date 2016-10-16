59fbb33609cc4aeed3f2bb9140e87bc672be3bc5
nesseb <japajaskin1994@gmail.com>
2016-10-16 22:44:39 +0200

    Commit

---
+++ SimpleExampleI/src/Zad5/Zad5.java
@@ -0,0 +1,13 @@
+package Zad5;
+
+/*
+ * Pobierz od usera dwa napisy i wypisz na ekranie ten, który posiada wiêcej cyfr.
+ */
+public class Zad5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
